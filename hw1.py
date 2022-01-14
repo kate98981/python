36519cb0062d6a833e5273cb4f5c5fba78cb89a7
kate98981 <orehova1998@list.ru>
2022-01-15 00:01:10 +0500

    Initial commit

---
+++ hw1.py
@@ -0,0 +1,31 @@
+a = "hello"
+b = 1
+c = 2.05
+d = b'Kate'
+e = ['hi', 12, 2.0]
+f = ("Kate", 23, 8 + 8j)
+g = {1, 2, 3, f, c}
+h = frozenset(['a', 'b', 'c'])
+dic = {1: 'cat', 2: 'dog'}
+
+print("a = ", a, type(a))
+print("b = ", b, type(b))
+print("c = ", c, type(c))
+print("d = ", d, type(d))
+print("e = ", e, type(e))
+print("f = ", f, type(f))
+print("g = ", g, type(g))
+print("h = ", h, type(h))
+print("dic = ", dic, type(dic))
+
+str1 = "hello"
+str2 = "world"
+print(str1 + " " + str2)
+
+str3 = "age"
+age = 23
+print(str3 + "," + str(age))
+
+str4 = "world"
+year = 2022
+print(str4 + "+" + str(year))
