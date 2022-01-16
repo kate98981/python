6f1a2aee392ff07edd2de81af9184f5a0c781381
kate98981 <orehova1998@list.ru>
2022-01-17 00:20:53 +0500

    python_2 HW_2

---
+++ hw2.py
@@ -0,0 +1,90 @@
+import math
+# Arithmetic
+item_1 = 9
+item_2 = 10
+result_sum = item_1 + item_2
+print(result_sum)
+
+result_subtr = item_2 - item_1
+print(result_subtr)
+
+result_multi = item_1 * item_2
+print(result_multi)
+
+result_exp = item_1 ** item_2
+print(result_exp)
+
+result_m_exp = math.pow(item_1, item_2)
+print(result_m_exp)
+
+result_s_root = item_1 ** 0.5
+print(result_s_root)
+
+result_m_s_root = math.sqrt(item_1)
+print(result_m_s_root)
+
+result_mp_s_root = math.pow(item_1, 0.5)
+print(result_mp_s_root)
+
+item_1 = 7.2
+item_2 = 6.4
+result_devision = item_1 / item_2
+print(result_devision)
+
+result_m_floor = math.floor(result_devision)
+print(result_m_floor)
+
+result_m_ceil = math.ceil(result_devision)
+print(result_m_ceil)
+
+result_int = int(result_devision)
+print(result_int)
+
+result_no_devision_loss = item_1 // item_2
+print(result_no_devision_loss)
+
+result_division_loss = item_1 % item_2
+print(result_division_loss)
+
+item_3 = 10
+item_3 += 10
+print(item_3)
+
+item_3 -= 5
+print(item_3)
+
+item_3 *= 3
+print(item_3)
+
+item_3 /= 3
+print(item_3)
+
+item_3 **= 2
+print(item_3)
+
+item_3 **= 0.5
+print(item_3)
+
+item_3 %= item_3
+print(item_3)
+# Boolean
+b_item_t = True
+b_item_f = False
+b_item_result_sum = b_item_t + b_item_f
+print(b_item_result_sum)
+
+b_item_relult_subtr = b_item_t - b_item_f
+print(b_item_relult_subtr)
+
+b_item_relult_multi = b_item_t * b_item_f
+print(b_item_relult_multi)
+
+# b_item_relult_division = b_item_t / b_item_f
+# print(b_item_relult_division)
+
+b_item_1_int = int(b_item_t)
+print(b_item_1_int)
+
+b_item_2_int = int(b_item_f)
+print(b_item_2_int)
+
