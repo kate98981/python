6647d3ffb47a796c9fa677f6eddb9372c22b9e61
kate98981 <orehova1998@list.ru>
2022-01-16 18:37:10 +0500

    changed hw1.py

--- hw1.py
+++ hw1.py
@@ -5,7 +5,7 @@ d = b'Kate'
 e = ['hi', 12, 2.0]
 f = ("Kate", 23, 8 + 8j)
 g = {1, 2, 3, f, c}
-h = frozenset(['a', 'b', 'c'])
+h = frozenset({'a', 'b', 'c'})
 dic = {1: 'cat', 2: 'dog'}
 
 print("a = ", a, type(a))
@@ -18,14 +18,14 @@ print("g = ", g, type(g))
 print("h = ", h, type(h))
 print("dic = ", dic, type(dic))
 
-str1 = "hello"
+str1 = "hello "
 str2 = "world"
-print(str1 + " " + str2)
+print(str1 + str2)
 
-str3 = "age"
+str3 = "age "
 age = 23
-print(str3 + "," + str(age))
+print(str3 + str(age))
 
-str4 = "world"
+str4 = "world "
 year = 2022
-print(str4 + "+" + str(year))
+print(str4 + str(year))
