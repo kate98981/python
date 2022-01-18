6eb0fcde406c277aa0106459d6fbdf7795610654
kate98981 <orehova1998@list.ru>
2022-01-19 00:46:01 +0500

    python HW_1

--- hw1.py
+++ hw1.py
@@ -1,30 +1,31 @@
-a = "hello"
-b = 1
-c = 2.05
-d = b'Kate'
-e = ['hi', 12, 2.0]
-f = ("Kate", 23, 8 + 8j)
-g = {1, 2, 3, f, c}
-h = frozenset({'a', 'b', 'c'})
-dic = {1: 'cat', 2: 'dog'}
+str_type = "hello"
+int_type = 1
+float_type = 2.05
+bytes_type = b'Kate'
+list_type = ['hi', 12, 2.0]
+type_turp = ("Kate", 23, 8 + 8j)
+set_type = {1, 2, 3, type_turp, float_type}
+frozenset_type = frozenset({'a', 'b', 'c'})
+dict_type = {1: 'cat', 2: 'dog'}
 
-print("a = ", a, type(a))
-print("b = ", b, type(b))
-print("c = ", c, type(c))
-print("d = ", d, type(d))
-print("e = ", e, type(e))
-print("f = ", f, type(f))
-print("g = ", g, type(g))
-print("h = ", h, type(h))
-print("dic = ", dic, type(dic))
+print("str_type = ", str_type, type(str_type))
+print("int_type = ", int_type, type(int_type))
+print("float_type = ", float_type, type(float_type))
+print("bytes_type = ", bytes_type, type(bytes_type))
+print("list_type = ", list_type, type(list_type))
+print("type_turp = ", type_turp, type(type_turp))
+print("set_type = ", set_type, type(set_type))
+print("frozenset_type = ", frozenset_type, type(frozenset_type))
+print("dict_type = ", dict_type, type(dict_type))
 
 str1 = "hello "
 str2 = "world"
-print(str1 + str2)
+str_res = str1 + str2
+print(str_res)
 
-str3 = "age "
+str3 = "age"
 age = 23
-print(str3 + str(age))
+print(str3 + "," + str(age))
 
 str4 = "world "
 year = 2022
